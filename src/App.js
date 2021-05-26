366e9605a91ef8b6a09bf03bc44e12ed60e28b02
cnnay <carlaalraccn2018@gmail.com>
2021-05-25 23:25:00 -0300

    ajuste no menu do projeto

--- src/App.js
+++ src/App.js
@@ -35,9 +35,6 @@ function adicionarFrutas(Event) {
         <div class="nav-wrapper teal lighten-2">
           <a href="#" class="brand-logo ">App list</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
           </ul>
         </div>
         </nav>
